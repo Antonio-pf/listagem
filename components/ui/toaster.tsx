0217c12a4b3ff5c90c6f32ca9df4efbb991463d5
Antonio-pf <antoniopfelipe03@gmail.com>
2026-01-29 21:07:59 -0300

    feature: melhorias no feedback de confirmação

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -13,14 +13,9 @@ import {
 export function Toaster() {
   const { toasts } = useToast()
 
-  // Separate toasts by position
-  const topToasts = toasts.filter(t => !t.position || t.position === 'top')
-  const centerToasts = toasts.filter(t => t.position === 'center')
-
   return (
     <ToastProvider>
-      {/* Top positioned toasts (default) */}
-      {topToasts.map(function ({ id, title, description, action, position, ...props }) {
+      {toasts.map(function ({ id, title, description, action, position, ...props }) {
         return (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
@@ -34,24 +29,7 @@ export function Toaster() {
           </Toast>
         )
       })}
-      <ToastViewport position="top" />
-
-      {/* Center positioned toasts */}
-      {centerToasts.map(function ({ id, title, description, action, position, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport position="center" />
+      <ToastViewport />
     </ToastProvider>
   )
 }
